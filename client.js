320387a6a6fb3497ca2c8aba1d7e813f519a5067
jeremy-cady <jeremy.cady@outlook.com>
2021-12-28 18:59:01 -0600

    button created and functioning

--- client.js
+++ client.js
@@ -2,4 +2,16 @@ console.log('in client.js');
 
 $(document).ready(onReady);
 
+let divCount = 0;
 
+function onReady() {
+    console.log('so freakin\' ready 🤘');
+    
+    $('#generateBtn').on('click', divGenerator);
+}
+
+
+function divGenerator() {
+    console.log('generate button clicked');
+    
+}
